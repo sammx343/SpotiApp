a5bc01dc10b963f9dc91eff2a6d84453e5ac6e49
Sebastian Mayor Molinares <smayor@redsis.com>
2018-05-24 18:17:29 -0500

    first commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styles: []
+})
+export class HomeComponent implements OnInit {
+
+  paises: any[] = []; 
+
+  constructor(private http: HttpClient) { 
+    console.log("Lllamado");
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+    .subscribe( (res: any) => {
+      this.paises = res;
+      console.log(res);
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
